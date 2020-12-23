4abd4fe9cb37c2ae941359d10839e4f8cc834061
scsx <4182349+scsx@users.noreply.github.com>
2020-12-23 21:54:25 +0000

    End of section 20 and course

--- 20 Speak Number Guessing Game - Speech Recognition/script.js
+++ 20 Speak Number Guessing Game - Speech Recognition/script.js
@@ -2,49 +2,46 @@ const msgEl = document.getElementById("msg");
 
 const randomNum = getRandomNumber();
 
-console.log("Number:", randomNum);
+console.log("Number: " + randomNum);
 
 window.SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
 
-let recognition = new window.SpeechRecognition();
+let rec = new window.SpeechRecognition();
 
 // Start recognition and game
-recognition.start();
+rec.start();
 
-// Capture user speak
 function onSpeak(e) {
     const msg = e.results[0][0].transcript;
-
     writeMessage(msg);
     checkNumber(msg);
 }
 
-// Write what user speaks
-function writeMessage(msg) {
-    msgEl.innerHTML = `
-    <div>You said: </div>
-    <span class="box">${msg}</span>
-  `;
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
 }
 
-// Check msg against number
+function writeMessage(msg) {
+    msgEl.innerHTML = `
+        <div>You said:</div>
+        <span class="box">${msg}</span>
+    `;
+}
+
 function checkNumber(msg) {
     const num = +msg;
 
-    // Check if valid number
     if (Number.isNaN(num)) {
-        msgEl.innerHTML += "<div>That is not a valid number</div>";
+        msgEl.innerHTML = "<div>That's not a number</div>";
         return;
     }
 
-    // Check in range
     if (num > 100 || num < 1) {
-        msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+        msgEl.innerHTML += "<div>Number must be bewteen 1 and 100</div>";
         return;
     }
 
-    // Check number
     if (num === randomNum) {
         document.body.innerHTML = `
       <h2>Congrats! You have guessed the number! <br><br>
@@ -58,19 +55,14 @@ function checkNumber(msg) {
     }
 }
 
-// Generate random number
-function getRandomNumber() {
-    return Math.floor(Math.random() * 100) + 1;
-}
-
 // Speak result
-recognition.addEventListener("result", onSpeak);
+rec.addEventListener("result", onSpeak);
 
-// End SR service
-recognition.addEventListener("end", () => recognition.start());
+// End SR
+rec.addEventListener("end", () => rec.start());
 
-document.body.addEventListener("click", (e) => {
-    if (e.target.id == "play-again") {
-        window.location.reload();
-    }
+document.body.addEventListener('click', e => {
+  if (e.target.id == 'play-again') {
+    window.location.reload();
+  }
 });
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://www.udemy.com/course/web-projects-with-vanilla-javascript/learn/lecture/
 All of the code for every project is available in the Github repo at:
 
 https://github.com/bradtraversy/vanillawebprojects
+
+# Browser compatibility
+https://caniuse.com/speech-recognition
